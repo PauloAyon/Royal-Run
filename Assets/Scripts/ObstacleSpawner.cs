8fcebb196dc01c98d29b675fc3b984f449a2f4cb
PauloAyon <paulo.ayon.delgado@gmail.com>
2025-10-02 12:53:54 -0600

    Arreglo de objetos, donde los elige randomente y agregamos una nueva variante de prefab.

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstacleSpawnTime = 1f;
+    [SerializeField] float spawnWidth = 4f;
+    [SerializeField] Transform obstacleParent;
+
 
     void Start()
     {
@@ -15,8 +18,11 @@ public class ObstacleSpawner : MonoBehaviour
     {
         while (true)
         {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
+            
             yield return new WaitForSeconds(obstacleSpawnTime);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
         }
     }
 }
